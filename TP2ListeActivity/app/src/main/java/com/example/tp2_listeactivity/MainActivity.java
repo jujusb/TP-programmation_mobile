4e141a8bb6c37bc66f1ad297c962eccb76b73ccb
Steeve Doppler <steeve.doppler@insa-rennes.fr>
2019-02-07 14:05:23 +0100

    Suppression des activity inutilisée

--- TP2ListeActivity/app/src/main/java/com/example/tp2_listeactivity/MainActivity.java
+++ TP2ListeActivity/app/src/main/java/com/example/tp2_listeactivity/MainActivity.java
@@ -3,16 +3,13 @@ package com.example.tp2_listeactivity;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
         this.mesDonnees = new ArrayList<>();
 
